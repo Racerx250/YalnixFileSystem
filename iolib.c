f8c172ec3580d5aa5f305e53ba6199a873cbd108
Logan's PC <loganlawrence250@gmail.com>
2018-04-17 19:52:07 -0500

    added sample1, a basic user program

--- iolib.c
+++ iolib.c
@@ -1,66 +1,100 @@
 #include <comp421/filesystem.h>
 #include <comp421/iolib.h>
+#include <comp421/yalnix.h>
 
+struct my_msg {
+	int fid;
+	int num_data;
+	char data[16]; 
+	void *addr_ptr;
+};
+
+/* FID 0 */
 int Open(char *pathname) {
 	return 0;
 }
 
-int Close(int fd) {
+/* FID 1 */
+int Close(int fd) { 
 	return 0;
 }
 
+/* FID 2 */
 int Create(char *pathname) {
+	TracePrintf(0, "cool?\n");
+	struct my_msg create_msg;//= (struct my_msg) malloc(sizeof(struct my_msg));
+	create_msg.fid = 2;
+	create_msg.num_data = 0;
+	// new_msg.data = "this is a test";
+
+	void *msg = &create_msg;
+
+	int send_result = Send(msg, 1);
+
 	return 0;
 }
 
+/* FID 3 */
 int Read(int fd, void *buf, int size) {
 	return 0;
 }
 
+/* FID 4 */
 int Write(int fd, void *buf, int size) {
 	return 0;
 }
 
+/* FID 5 */
 int Seek(int fd, int offset, int whence) {
 	return 0;
 }
 
+/* FID 6 */
 int Link(char *oldname, char *newname) {
 	return 0;
 }
 
+/* FID 7 */
 int Unlink(char *pathname) {
 	return 0;
 }
 
+/* FID 8 */
 int SymLink(char *oldname, char *newname) {
 	return 0;
 }
 
+/* FID 9 */
 int ReadLink(char *pathname, char *buf, int len) {
 	return 0;
 }
 
+/* FID 10 */
 int MkDir(char *pathname) {
 	return 0;
 }
 
+/* FID 11 */
 int RmDir(char *pathname) {
 	return 0;
 }
 
+/* FID 12 */
 int ChDir(char *pathname) {
 	return 0;
 }
 
+/* FID 13 */
 int Stat(char *pathname, struct Stat *statbuf) {
 	return 0;
 }
 
+/* FID 14 */
 int Sync(void) {
 	return 0;
 }
 
+/* FID 15 */
 int Shutdown(void) {
 	return 0;
 }
--- yfs.c
+++ yfs.c
@@ -1,13 +1,79 @@
 #include <comp421/yalnix.h>
 #include <comp421/iolib.h>
+#include <comp421/filesystem.h>
+#include <stdio.h>
+
+#define NODES_PER_BLOCK BLOCKSIZE / INODESIZE
+
+struct inode *node_list;
+
+int total_inodes;
+int total_blocks;
+
+typedef struct free_block {
+    int num;
+    struct free_block *next;
+} FBlock;
+
+typedef struct free_node {
+    int num;
+    struct free_node *next;
+} FNode;
+
+FBlock *fbh;
+FNode *fnh;
 
 int
 main()
 {
-	TracePrintf(0, "[Server Process] Starting...\n");
-	int result = Register(1);
-	TracePrintf(0, "[Server Process] Waiting on message...\n");
-	while (1) {
-		/* RECEIVE LOGIC */
-	}
+    
+    
+    int result = Register(1);
+    struct fs_header *header = (struct fs_header *) malloc(sizeof(struct fs_header));
+    size_t buf[SECTORSIZE];
+    int i, j;
+
+    fbh = (FBlock *) malloc(sizeof(FBlock));
+    fnh = (FNode *) malloc(sizeof(FNode));
+
+    TracePrintf(0, "[Server Process] Starting...\n");
+
+    ReadSector(1, buf);
+    memcpy(header, buf, sizeof(struct fs_header));
+    total_inodes = header->num_inodes;
+    total_blocks = header->num_blocks;
+    //free(buf);
+    printf("num_inodes %i, num_blocks %i\n", total_inodes, total_blocks);
+	
+    node_list = malloc(sizeof(struct inode) * (total_inodes + 1));
+	
+    for (i = 2; i < total_inodes + 1; i++) {
+		node_list[i].type = INODE_FREE;
+		node_list[i].nlink = 0;
+
+		node_list[i].reuse = 0;
+		node_list[i].size = 0;
+		for (j = 0; j < NUM_DIRECT; j++) {
+		    node_list[i].direct[j] = 0;
+		}
+		node_list[i].indirect = 0;
+
+		FNode *temp = fnh;
+		while (temp->next != NULL) {
+		    temp = temp->next;
+		}
+		temp->next = (FNode *) malloc(sizeof(FNode));
+		temp->num = i;
+    }
+	
+	void *msg = malloc(8*sizeof(char));
+	
+    TracePrintf(0, "[Server Process] Waiting on message... \n");
+
+    while (1) {
+    	int pid = Receive(msg);
+    	if (pid != 0) {
+    		TracePrintf(0, "[Server Process] received message\n");
+    	}
+    }
 }
